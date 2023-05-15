cdbd1c7902742009345f608d1bdc79093403e3b3
Daniele Fea <daniele.fea@vallauri.edu>
2023-05-15 09:57:35 +0200

    Correzione dell'indirizzo per la richiesta delle coordinate

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
 var map;
 
 window.onload = async function(){
-    let busta = await fetch("https://www.openstreetmap.org/search?format=json&city=Fossano")
+    let busta = await fetch("https://nominatim.openstreetmap.org/search?format=json&city=Fossano");
+
 
 
     //Definisco una mappa
-    /*
     map = new ol.Map(
         {
             target:"map", /* id dell'oggetto html */
             /* Definisco il livello base (mappa globale completa) */
-            /*layers:[
+            layers:[
                 new ol.layer.Tile({source:new ol.source.OSM()})
             ],
-            /* caratteristiche visive (zoom, centro, ...) della mappa creata */
-            /*view:new ol.View({
-
+            /*caratteristiche visive (zoom, centro, ...) della mappa creata */
+            view:new ol.View({
+                
                 zoom:4
             })
-    });*/
+    });
 }
