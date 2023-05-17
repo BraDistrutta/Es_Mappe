da256bf37ecd4923b0b49912efca61292950ecfc
BraDistrutta <lucktesta9@gmail.com>
2023-05-17 13:17:46 +0200

    siuuuuu

--- js/index.js
+++ js/index.js
@@ -1,22 +1,54 @@
+ol.proj.useGeographic();
 var map;
 
 window.onload = async function(){
     let busta = await fetch("https://nominatim.openstreetmap.org/search?format=json&city=Fossano");
+    let bustaJSON = await busta.json();
+    console.log(bustaJSON[0]);
 
 
+    let coords = [parseFloat(bustaJSON[0].lon), parseFloat(bustaJSON[0].lat)];
 
-    //Definisco una mappa
     map = new ol.Map(
         {
-            target:"map", /* id dell'oggetto html */
-            /* Definisco il livello base (mappa globale completa) */
+            target:"map",
             layers:[
                 new ol.layer.Tile({source:new ol.source.OSM()})
             ],
-            /*caratteristiche visive (zoom, centro, ...) della mappa creata */
             view:new ol.View({
-                
-                zoom:4
+                zoom:14,
+                center: [coords[0],coords[1]]
             })
     });
+
+    let layer1 = createLayer(map, "../img/marker3.png");
+    createMarker(layer1,"zemel", coords[0], coords[1]);
+
+    console.log(map);
+    map.on('click', function(event) {
+        console.log(event.coordinate);
+        createMarker(layer1,"neger", event.coordinate[0], event.coordinate[1]);
+    });
+}
+
+function createLayer(map, pathImg) {
+    let layer = new ol.layer.Vector({
+        source: new ol.source.Vector(),
+        style: new ol.style.Style({
+            image: new ol.style.Icon({
+                anchor: [0.5, 1],
+                src: pathImg
+            })
+        }),
+        name: "zebby"
+    });
+
+    map.addLayer(layer);
+    return layer;
+}
+
+function createMarker(layer, MKname, lon, lat) {
+    let marker = new ol.Feature(new ol.geom.Point([lon, lat]));
+    marker.name = MKname;
+    layer.getSource().addFeature(marker);
 }
