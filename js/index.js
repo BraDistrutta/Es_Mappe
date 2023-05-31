fade388fe08c73227ffb97a71d3ced0b32ddb874
BraDistrutta <lucktesta9@gmail.com>
2023-05-31 09:33:23 +0200

    siuuuuu

--- js/index.js
+++ js/index.js
@@ -1,8 +1,23 @@
 ol.proj.useGeographic();
-var map;
-var vet = new Array('Chicago', 'Pechino', 'Napoli','Foggia','Tokio','Catanzaro','Atlanta','Toronto','Houston','Padova');
+var map, modal, testoModal;
+var datiFoss={nome: "Fossano", desc:"La città più grande di Italia (dopo Cappellazzo)"};
+var dati = [
+    {nome: "Chicago", desc: "Io invece no"},
+    {nome: "Pechino", desc: "Pochino di cosa?"},
+    {nome: "Napoli", desc: "VESUVIO ERUTTA, TUTTA NAPOLI E' DISTRUTTA"},
+    {nome: "Foggia", desc: "WTF is goin' on in Foggia?"},
+    {nome: "Tokio", desc: "Deja vu, i've just been in this place before, ridin on the streets"},
+    {nome: "Catanzaro", desc: "La grande foresta amazzonica Italiana (a malapena)"},
+    {nome: "Atlanta", desc: "Boh in pratica l'Atalanta non lo so non guardo il calcio"},
+    {nome: "Toronto", desc: "W I N D    W I N D    W I N D     W I N D     W I N D"},
+    {nome: "Houston", desc: "La casa del boss della barba (ha minacciato di uccidere la mia famiglia se non l'avessi detto)"},
+    {nome: "Padova", desc: "E' molto lontana l'Islanda, è vicina a Padova l'Islanda"}
+];
+var datiAmog = {nome: "Amogus", desc:"PAM PAM PAM PAM PAM PAM PAM, PA-PA-PAM, PAM PAM"};
 
 window.onload = async function(){
+    modal = document.getElementById("sfondoModal");
+    testoModal = document.querySelector("#myModal main");
     let busta = await fetch("https://nominatim.openstreetmap.org/search?format=json&city=Fossano");
     let bustaJSON = await busta.json();
     console.log(bustaJSON[0]);
@@ -23,22 +38,31 @@ window.onload = async function(){
     });
 
     let layer1 = createLayer(map, "../img/marker3.png");
-    createMarker(layer1,"zemel", coords[0], coords[1]);
+    createMarker(layer1,datiFoss, coords[0], coords[1]);
 
     console.log(map);
     map.on('click', function(event) {
-        console.log(event.coordinate);
-        createMarker(layer1,"neger", event.coordinate[0], event.coordinate[1]);
+        let marker = map.forEachFeatureAtPixel(event.pixel, function(feature){return feature});
+        if(marker!=undefined){
+            console.log(event.pixel);
+            console.log(marker);
+            testoModal.innerHTML = marker.dati.nome + "<br>" + marker.dati.desc;
+            modal.style.display = "flex";
+        }
+        else{
+            console.log(event.coordinate);
+            createMarker(layer1,datiAmog, event.coordinate[0], event.coordinate[1]);
+        }
     });
 
-    for(let i=0;i<bustaJSON.length;i++){
-        let busta = await fetch("https://nominatim.openstreetmap.org/search?format=json&city="+vet[i]);
+    for(let i=0;i<dati.length;i++){
+        let busta = await fetch("https://nominatim.openstreetmap.org/search?format=json&city="+dati[i].nome);
         let bustaJSON = await busta.json();
         console.log(bustaJSON[0]);
 
         let coords = [parseFloat(bustaJSON[0].lon), parseFloat(bustaJSON[0].lat)];
         let layer1 = createLayer(map, "../img/marker3.png");
-        createMarker(layer1,"zemel", coords[0], coords[1]);
+        createMarker(layer1,dati[i], coords[0], coords[1]);
     }
 }
 
@@ -58,8 +82,11 @@ function createLayer(map, pathImg) {
     return layer;
 }
 
-function createMarker(layer, MKname, lon, lat) {
+function createMarker(layer, data, lon, lat) {
     let marker = new ol.Feature(new ol.geom.Point([lon, lat]));
-    marker.name = MKname;
+    marker.name = data.nome;
+    marker.dati = data;
+    marker.lon = lon;
+    marker.lat = lat;
     layer.getSource().addFeature(marker);
 }
